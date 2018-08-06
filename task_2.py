cdb3ef238cd59bc3fad5edd0a1262c74f55dae9c
lisam <lisa.mohonko@gmail.com>
2018-08-06 22:55:31 +0300

    Task 2 is completed

---
+++ task_2.py
@@ -0,0 +1,10 @@
+#Task #2
+#Найти результат выражения для произвольных значений a,b: (a2 + b2) % 2
+
+import math
+a = 3
+b = 6
+ab_result = (pow(a, 2) + pow(b, 2))%2
+
+#Need to know how to put % sign in such string
+print('Result of (a**2 + b**2)%2', 'for a = %.2f and b = %.2f is %d' %(a, b, ab_result))
