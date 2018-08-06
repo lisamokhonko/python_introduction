6cb144654e14b47d9fa8984ed956f3b98ce6feac
lisam <lisa.mohonko@gmail.com>
2018-08-06 22:08:31 +0300

    Task 1 is completed

--- task_1.py
+++ task_1.py
@@ -1,5 +1,8 @@
-print('Hello World')
+#Task #1
+#Найти результат выражения для произвольных значений a,b,c: a + b * ( c / 2 )
 
-
-
-dbcsdb
+a = 10
+b = 20
+c = 30
+abc_result = a + b*(c/2)
+print('Result of a + b * ( c / 2 ) for a = %.2f, b = %.2f, c = %.2f is %.2f' %(a, b, c, abc_result))
