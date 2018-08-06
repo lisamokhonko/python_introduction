0e6101b63b2d015f3b4f58694c5495dc9ca35e87
lisam <lisa.mohonko@gmail.com>
2018-08-06 22:56:13 +0300

    Task 3 is completed

---
+++ task_3.py
@@ -0,0 +1,9 @@
+#Task #3
+#Найти результат выражения для произвольных значений a,b,c: ( a + b ) / 12 * c % 4 + b
+
+a = 20
+b = 30
+c = 1
+abc_result = (a + b)/12*c%4 + b
+
+print('Result of ( a + b )/12*c%4 + b','for a = %.2f, b = %.2f, c = %.2f is %.5f' %(a, b, c, abc_result))
