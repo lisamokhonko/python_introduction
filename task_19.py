c294d06feb09aee614cf1afd9132153f4236b524
lisam <lisa.mohonko@gmail.com>
2018-09-02 13:27:41 +0300

    Task 19 has been fixed.

--- task_19.py
+++ task_19.py
@@ -5,18 +5,26 @@
 import random
 
 def diff_min_max(num_limit, lower_bound, upper_bound): # returns int
-    num_min = random.randint(lower_bound, upper_bound)
-    num_max = 0
     result = 0
-    current_value = 0
     for i in range(num_limit):
-        current_value = random.randint(lower_bound, upper_bound)
-        if num_min >= current_value:
+        if i != 0:
+            current_value = random.randint(lower_bound, upper_bound)
+            print(current_value)
+            if num_min > current_value:
+                num_min = current_value
+            if num_max < current_value:
+                num_max = current_value
+        elif i == 0:
+            current_value = random.randint(lower_bound, upper_bound)
+            print('First value', current_value)
             num_min = current_value
-        if num_max <= current_value:
             num_max = current_value
-        print(current_value)
+    print(num_min, num_max)
     result = num_max - num_min
     return result
 
-print('Difference is:',diff_min_max(5, 1, 10))
+print('Difference is:',diff_min_max(10, 100, 200))
+
+print('Difference is:',diff_min_max(10, -200, -100))
+
+#print(random.randint(-200, -100))
