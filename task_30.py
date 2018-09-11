563fef8558c99354f7907a0036dc00aa41b587e0
lisam <lisa.mohonko@gmail.com>
2018-09-11 22:28:26 +0300

    Task 30 has been fixed.

--- task_30.py
+++ task_30.py
@@ -1,13 +1,14 @@
 #Task #30
 #Написать функцию, возвращающую все простые числа от 1 до 100 в виде списка.
 def gen_primes(): # returns list of ints
-    lst = [1]
+    lst = [2]
     #print(type(lst))
-    for num in range(2, 101):
+    for num in range(3, 101):
         prime = True
         for i in range(2, num):
             if (num%i == 0):
                 prime = False
+                break
         if prime:
            lst.append(num)
     return lst
