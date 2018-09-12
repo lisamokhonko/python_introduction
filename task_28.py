34ab02d2e6441038b59a8c53eb4792a179113d4e
lisam <lisa.mohonko@gmail.com>
2018-09-12 22:17:41 +0300

    Task 28 has been fixed.

--- task_28.py
+++ task_28.py
@@ -5,28 +5,29 @@
 #Таблица шифрования задается программистом в виде одномерного списка символов латинского алфавита от a до z и цифр от 0 до 9.
 #Если при выборе символа для шифровки таблица шифрования заканчивается, то циклически переходить к ее началу.
 #Отсутствующие в таблице шифрования символы, записываются в результирующую строку без изменений. Регистр игнорируется.
-	# Таблица шифрования (a, b, c, d, ..., x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-	# Например: Исходная строка, которую ввел пользователь: 'secret', 'office 365'
-	# Зашифрованная строка, которую выдала программа: 'xjhwjy', 'tkknhj 8ba'
-	# Примечание: т.н. таблица шифрования может быть представлена как строка или список.
+# Таблица шифрования (a, b, c, d, ..., x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+# Например: Исходная строка, которую ввел пользователь: 'secret', 'office 365'
+# Зашифрованная строка, которую выдала программа: 'xjhwjy', 'tkknhj 8ba'
+# Примечание: т.н. таблица шифрования может быть представлена как строка или список.
+import re
 
-str_to_encode = 'abc'
+str_to_encode = 'office 365'
 
 def encode(str_to_encode): # returns enсoded string
-    encryption_table = 'a, b, c, d, e, f, j, h, i, j, k, l, m'#, n, o, p, q, r, s, t, u, v, w, x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
-    encryption_table_lst = encryption_table.split(',')
-    str_decoded = ''
-    print(encryption_table_lst)
-    n = len(encryption_table_lst)
-    for char in str_to_encode:
-        if char not in encryption_table_lst:
-            str_decoded += char
-        else:
-            for i in range(n):
-                #print(encryption_table_lst[i])
-                if char == encryption_table_lst[i]:
-                    str_decoded += encryption_table_lst[i + 5]
-                    break
-    return str_decoded
+   encryption_table = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+   encryption_table = re.sub(' ', '', encryption_table)
+   encryption_table_lst = encryption_table.split(',')
+   str_decoded = ''
+   #print(encryption_table_lst)
+   n = len(encryption_table_lst)
+   for char in str_to_encode:
+       if char not in encryption_table_lst:
+           str_decoded += char
+       else:
+           for i in range(n):
+               if char == encryption_table_lst[i]:
+                   new_idx = (i + 5) % n
+                   str_decoded += encryption_table_lst[new_idx]
+   return str_decoded
 
 print(encode(str_to_encode))
